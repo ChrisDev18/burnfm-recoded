1224df32209c1edd556febe9a1b467452d35572e
Chris W <95181085+ChrisDev18@users.noreply.github.com>
2024-01-26 03:47:04 +0000

    Split up RadioPlayer and Player stylesheets
    Created new Show.module.css file.
    Rearranged RadioPlayer.module.css file.
    Removed global (within RadioPlayer) styling for p, as it interfered with styling overwrites.

--- src/app/ui/Show.tsx
+++ src/app/ui/Show.tsx
@@ -1,4 +1,4 @@
-import styles from "@/app/ui/RadioPlayer.module.css";
+import styles from "@/app/ui/Show.module.css";
 import Image from "next/image";
 import fallback from "../../../public/Radio-Microphone.png";
 import {Show} from "@/app/lib/types";
