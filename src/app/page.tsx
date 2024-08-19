1d5d20f7f27d30aa413a252ed8409781a18bee91
Chris W <95181085+ChrisDev18@users.noreply.github.com>
2024-08-19 23:58:19 +0100

    Make Instagram vector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import buttons from '@/app/styles/buttons.module.css'
 import './styles/icons.css';
 import Image from "next/image";
 import SpotifyIcon from "../../public/Spotify_icon.svg"
-import InstagramIcon from "../../public/Instagram_icon.png"
+import InstagramIcon from "../../public/Instagram_icon.svg"
 import PodcastsIcon from "../../public/Podcasts_icon.svg"
 import Link from "next/link";
 import React from "react";
@@ -26,6 +26,7 @@ export default function Home() {
       <RadioPlayer/>
 
       <div className={styles.Cards}>
+
         <div className={styles.CardWrapper}>
           <div className={styles.Card} style={{background: "#32103F"}}>
             <h2>BurnFM on Instagram</h2>
@@ -35,7 +36,7 @@ export default function Home() {
             </p>
 
             <div className={styles.cardButtons}>
-              <Link className={buttons.Button} href={"https://www.instagram.com/burn_fm/"}>
+              <a className={buttons.Button} href={"https://www.instagram.com/burn_fm/"}>
                 <Image
                     src={InstagramIcon}
                     alt={"Instagram icon"}
@@ -43,7 +44,7 @@ export default function Home() {
                     width={28}
                 />
                 Visit our Instagram
-              </Link>
+              </a>
             </div>
           </div>
 
@@ -57,18 +58,93 @@ export default function Home() {
         </div>
 
         <div className={styles.CardWrapper}>
+          <div className={styles.Card} style={{background: '#5D1F74'}}>
+            <h2>Burn Podcasts</h2>
+            <p>
+              BurnFM doesn&apos;t just do radio - we also have a large and vibrant community of podcasters.
+            </p>
+
+            <div className={styles.cardButtons}>
+              <a className={buttons.Button} href={"https://open.spotify.com/show/0ALexnN0yS3OX4xdiPetic"}>
+                <Image
+                    src={SpotifyIcon}
+                    alt={"Spotify icon"}
+                    height={28}
+                    width={28}
+                />
+                Listen on Spotify
+              </a>
+              <a className={buttons.Button} href={"https://podcasts.apple.com/us/podcast/burn-fm/id1521913304"}>
+                <Image
+                    src={PodcastsIcon}
+                    alt={"Apple Podcasts icon"}
+                    height={28}
+                    width={28}
+                />
+                Listen on Apple Podcasts
+              </a>
+            </div>
+          </div>
+          <div className={styles.CardIcon}>
+            <span className={"material-symbols-rounded notranslate"} style={{color: "#5D1F74"}}>
+              podcasts
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.CardWrapper}>
+          <div className={styles.Card} style={{background: '#1D1D1D'}}>
+            <h2>BurnFM Mixes on Spotify</h2>
+            <p>
+              Music and Radio go hand in hand, so we made a Spotify account! Follow us on @theburnfm and:
+            </p>
+            <ul>
+              <li>discover new sub-genres of music listening to <i>BurnFM Introduces...</i></li>
+              <li>play back the songs from our past Media Balls,</li>
+              <li>and explore all the other playlists from the past, curated here at BurnFM.</li>
+            </ul>
+            <a className={buttons.Button} href={"https://open.spotify.com/user/theburnfm"}>
+              <Image
+                  src={SpotifyIcon}
+                  alt={"Spotify icon"}
+                  height={28}
+                  width={28}
+              />
+              Discover our Spotify mixes
+            </a>
+          </div>
+          <Image
+              className={styles.CardIcon}
+              src={SpotifyIcon}
+              alt={"Spotify icon"}
+              height={64}
+              width={64}
+          />
+        </div>
+
+        <div className={styles.CardWrapper}>
           <div className={styles.Card} style={{background: "#5D1F74"}}>
             <h2>Interested in joining?</h2>
             <p>
-              We are UoB&apos;s official radio station. If you are interested in radio or podcasting (no matter your
-              experience... seriously), this is the society for you!
+              We are the University of Birmingham&apos;s official radio station. If you are interested in radio or
+              podcasting (no matter your
+              experience), this is the society for you!
               {/*Many of our members are newbies to radio and podcasting, so don&apos;t feel you need to be an expert in*/}
               {/*anything.*/}
             </p>
             <p>
-              If you are looking at new societies to join during the coming year at UoB, please come see us at the societies
+              If you are looking for new societies to join during the coming year at UoB, please come see us at the
+              societies
               fair on <u>Friday 27th September 2024</u>.
             </p>
+            <div className={styles.cardButtons}>
+              <a href={'https://www.guildofstudents.com/studentgroups/societies/burnfm/'} className={buttons.Button}>
+                Our Guild of Students page
+              </a>
+              <a href={'https://www.guildofstudents.com/welcome/events/event/9374/'} className={buttons.Button}>
+                Info about Societies Fair
+              </a>
+            </div>
           </div>
 
           <div className={styles.CardIcon}>
@@ -108,9 +184,11 @@ export default function Home() {
               Running a radio station is no small feat. See the team who lead our station, as well as the managers of
               our fantastic sections.
             </p>
-            <Link href={'/about'} className={buttons.Button}>
-              More about us
-            </Link>
+            <div className={styles.cardButtons}>
+              <Link href={'/about'} className={buttons.Button}>
+                More about us
+              </Link>
+            </div>
           </div>
 
           <div className={styles.CardIcon}>
@@ -119,70 +197,6 @@ export default function Home() {
             </span>
           </div>
 
-        </div>
-
-        <div className={styles.CardWrapper}>
-          <div className={styles.Card} style={{background: '#5D1F74'}}>
-            <h2>Burn Podcasts</h2>
-            <p>
-              BurnFM doesn&apos;t just do radio - we also have a large and vibrant community of podcasters.
-            </p>
-
-            {/*<div className={styles.cardButtons}>*/}
-              <Link className={buttons.Button} href={"https://open.spotify.com/show/0ALexnN0yS3OX4xdiPetic"}>
-                <Image
-                    src={SpotifyIcon}
-                    alt={"Spotify icon"}
-                    height={28}
-                    width={28}
-                />
-                Listen on Spotify
-              </Link>
-              <Link className={buttons.Button} href={"https://podcasts.apple.com/us/podcast/burn-fm/id1521913304"}>
-                <Image
-                    src={PodcastsIcon}
-                    alt={"Apple Podcasts icon"}
-                    height={28}
-                    width={28}
-                />
-                Listen on Apple Podcasts
-              </Link>
-            {/*</div>*/}
-          </div>
-          <div className={styles.CardIcon}>
-            <span className={"material-symbols-rounded notranslate"} style={{color: "#5D1F74"}}>
-              podcasts
-            </span>
-          </div>
-        </div>
-
-        <div className={styles.CardWrapper}>
-          <div className={styles.Card} style={{background: '#1D1D1D'}}>
-            <h2>BurnFM on Spotify</h2>
-            <p>
-              Music and Radio go hand in hand, so we made a Spotify account!
-            </p>
-            <p>
-              Follow us on @theburnfm to listen to our playlists,
-              keep up to date with new releases, and listen back to our ball playlists!
-            </p>
-            <Link className={buttons.Button} href={"https://open.spotify.com/user/theburnfm"}>
-              <Image
-                  src={SpotifyIcon}
-                  alt={"Spotify icon"}
-                  height={28}
-                  width={28}
-              />
-              Visit our Spotify profile
-            </Link>
-          </div>
-          <Image
-              className={styles.CardIcon}
-              src={SpotifyIcon}
-              alt={"Spotify icon"}
-              height={64}
-              width={64}
-          />
         </div>
 
       </div>
