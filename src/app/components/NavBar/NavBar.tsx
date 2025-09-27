45b253edb252d3b82b87b53000f67c9555519881
Chris W <95181085+ChrisDev18@users.noreply.github.com>
2025-09-27 02:34:53 +0100

    Upgrade Next.js to 15.5.4

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -7,7 +7,6 @@ import {usePathname} from "next/navigation";
 import BurnLogo from "@/app/components/BurnLogo";
 import {
   CalendarIcon,
-  HeadphonesIcon,
   LayoutGridIcon, MessageCircleQuestion,
   UsersRoundIcon
 } from "lucide-react";
